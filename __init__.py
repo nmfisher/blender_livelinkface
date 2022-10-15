86f566b3e82c824debb3e5ffe580609c9f6169fd
Nick Fisher <nick.fisher@avinium.com>
2022-10-15 18:09:24 +1100

    add multiple targets and begin work on panel

--- __init__.py
+++ __init__.py
@@ -8,26 +8,50 @@ bl_info = {
 
 import bpy
 
-from livelinkface.operators import LiveLinkFacePanel, ConnectOperator, LoadCSVOperator
+from livelinkface.operators import LiveLinkFacePanel, ConnectOperator, LoadCSVOperator, CUSTOM_OT_actions, CUSTOM_OT_addViewportSelection, CUSTOM_OT_printItems, CUSTOM_OT_clearList, CUSTOM_OT_removeDuplicates, CUSTOM_OT_selectItems, CUSTOM_OT_deleteObject, CUSTOM_UL_items
 
-classes = [ LiveLinkFacePanel, ConnectOperator, LoadCSVOperator ]
+class ObjectSlot(bpy.types.PropertyGroup):
+    obj: bpy.props.PointerProperty(name="Object",type=bpy.types.Object)
+
+
+classes = (
+    LiveLinkFacePanel, 
+    ConnectOperator, 
+    LoadCSVOperator, 
+    ObjectSlot,
+    CUSTOM_OT_actions,
+    CUSTOM_OT_addViewportSelection,
+    CUSTOM_OT_printItems,
+    CUSTOM_OT_clearList,
+    CUSTOM_OT_removeDuplicates,
+    CUSTOM_OT_selectItems,
+    CUSTOM_OT_deleteObject,
+    CUSTOM_UL_items,
+    
+)
+
+
         
 def register():
+    for c in classes:
+        bpy.utils.register_class(c)
     bpy.types.Scene.ll_is_listening = bpy.props.BoolProperty(name="Server listening", description="Whether the server is currently listening", default=False)
     bpy.types.Scene.ll_host_ip = bpy.props.StringProperty(name="Host IP", description="IP address of the interface on this machine to listen", default="0.0.0.0")
     bpy.types.Scene.ll_host_port = bpy.props.IntProperty(name="Port", description="Port", default=11111)
-    bpy.types.Scene.ll_target = bpy.props.PointerProperty(
-        type = bpy.types.Object,
-        name = "Target object to attach the LiveLink stream to",
+    bpy.types.Scene.ll_targets = bpy.props.CollectionProperty(
+        type = ObjectSlot,
+        name = "Target object(s) to attach the LiveLink stream to",
     )
+    bpy.types.Scene.ll_targets_index = bpy.props.IntProperty()
 
-    for c in classes:
-        bpy.utils.register_class(c)
+
 
 
 def unregister():
     for c in classes:
         bpy.utils.unregister_class(c)
+    del bpy.types.Scene.ll_targets
+    del bpy.types.Scene.ll_target_index
         
 if __name__ == "main":
     register()        
--- bpylivelinkface.py
+++ bpylivelinkface.py
@@ -5,14 +5,16 @@ import csv
 
 from livelinkface.pylivelinkface import PyLiveLinkFace, FaceBlendShape
 
+LIVE_LINK_FACE_HEADER = "Timecode,BlendShapeCount,EyeBlinkLeft,EyeLookDownLeft,EyeLookInLeft,EyeLookOutLeft,EyeLookUpLeft,EyeSquintLeft,EyeWideLeft,EyeBlinkRight,EyeLookDownRight,EyeLookInRight,EyeLookOutRight,EyeLookUpRight,EyeSquintRight,EyeWideRight,JawForward,JawRight,JawLeft,JawOpen,MouthClose,MouthFunnel,MouthPucker,MouthRight,MouthLeft,MouthSmileLeft,MouthSmileRight,MouthFrownLeft,MouthFrownRight,MouthDimpleLeft,MouthDimpleRight,MouthStretchLeft,MouthStretchRight,MouthRollLower,MouthRollUpper,MouthShrugLower,MouthShrugUpper,MouthPressLeft,MouthPressRight,MouthLowerDownLeft,MouthLowerDownRight,MouthUpperUpLeft,MouthUpperUpRight,BrowDownLeft,BrowDownRight,BrowInnerUp,BrowOuterUpLeft,BrowOuterUpRight,CheekPuff,CheekSquintLeft,CheekSquintRight,NoseSneerLeft,NoseSneerRight,TongueOut,HeadYaw,HeadPitch,HeadRoll,LeftEyeYaw,LeftEyePitch,LeftEyeRoll,RightEyeYaw,RightEyePitch,RightEyeRoll".split(",")
+
 instance = None
 
 ''' Create an instance of an BlenderLiveLinkFaceAnimator that will listen on the given IP/port. Prefer using this method than constructing an instance directly as this will ensure that any pre-existing connections are closed'''
-def create_instance(target, host= "0.0.0.0", port = 11111):
+def create_instance(targets, host= "0.0.0.0", port = 11111):
     global instance
     if instance is not None:
         instance.close()
-    instance = LiveLinkFaceServer(target, host, port)
+    instance = LiveLinkFaceServer(targets, host, port)
     
 class LiveLinkTarget:
 
@@ -27,95 +29,21 @@ class LiveLinkTarget:
         self.bone_props = [] 
         
         # iterate over LIVE_LINK_HEADER and extract any ARKit blendshape names that are driven by bones in the target object
-        for i in range(LIVE_LINK_HEADER - 2):
+        for i in range(len(LIVE_LINK_FACE_HEADER) - 2):
             bone_prop = self.livelink_to_bone_prop(i)
             if bone_prop is not None:
-                bone_props += [bone_prop]
-                
+                self.bone_props += [bone_prop]
+#        print(f"{len(self.bone_props)} bone props") 
         self.bone_frames = [[0] * len(self.bone_props)]*num_frames
                 
         if action_name is not None:
             self.create_action(action_name)
             
-    '''Sets the value for the LiveLink blendshape at index [i_ll] to [val] for frame [frame] (note the underlying target may be a blendshape or a bone).'''
-    def set_frame_value(self, i_ll, val, frame=0):
-        i_sk = self.livelink_to_shapekey_idx(i_ll)
-        bone_idx = 0
-        if i_sk != -1:
-            self.sk_frames[frame][i_sk] = value
-        else:
-            bone_prop = self.livelink_to_bone_prop(i_ll)
-            if bone_prop is not None:
-                self.bone_frames[frame][bone_idx] = val
-                bone_idx += 1
-            else:
-            #    print(f"Failed to set bone prop for {i_ll}")
-                pass
-
-    '''Loads a CSV in LiveLinkFace format. First line is the header (Timecode,BlendshapeCount,etc,etc), every line thereafter is a single frame with comma-separated weights'''
-    @staticmethod
-    def fromcsv(self,target,path,use_first_frame_as_zero=False, action_name):        
-        csvdata = [x for x in csv.reader(open(path,"r"))]
-        num_frames = len(csvdata) - 1
-        
-        target = LiveLinkTarget(target, num_frames, action_name=action_name)
-
-        for blendshape,idx in enumerate(LIVE_LINK_FACE_HEADER):
-            if idx == 0:
-                continue
             
-            rest_weight = float(csvdata[1][idx])
-            
-            for i in range(1, num_frames):
-                val = float(csvdata[i][idx])
-                if self.use_first_frame_as_zero:
-                    val -= rest_weight
-                self.set_frame_value(idx, i-1, val)
-        
-        target.update_animation()
-    
-    def update_animation(self):
-        # a bit slow to use bpy.context.object.data.shape_keys.keyframe_insert(datapath,frame=frame)
-        # (where datapath is 'key_blocks["MouthOpen"].value') 
-        # better to add a new fcurve for each shape key then set the points in one go        
-        for i_sk,fc in enumerate(self.sk_fcurves):
-            frame_data = [i,self.sk_frames[i][i_sk] for i in len(self.sk_frames)]
-            fc.keyframe_points.foreach_set('co',frame_data)
-       
-    def create_action(self, action_name):
-    
-        # create a new Action so we can directly create fcurves and set the keyframe points
-        self.sk_action = bpy.data.actions.new(f"action_name) 
-    
-        # create the bone AnimData if it doesn't exist 
-        if target.animation_data is None:
-            target.animation_data_create()
-            
-        target.data.shape_keys.animation_data.action = self.action
-            
-        # create the shape key AnimData if it doesn't exist 
-        if target.data.shape_keys.animation_data is None:
-            target.data.shape_keys.animation_data_create()
-            
-        target.data.shape_keys.animation_data.action = self.action
-        
-        self.sk_fcurves = []
-        
-        for sk in target.data.shape_keys.key_blocks:
-            datapath = f"{sk.path_from_id()}.value"
-            fc = action.fcurves.new(datapath)
-            fc.keyframe_points.add(count=len(self.sk_frames))
-            self.sk_fcurves += [fc]
-            
-        for bone_prop in self.bone_props:
-            fc = action.fcurves.new(datapath)
-        
-        for bone_prop in 
-    
-    def livelink_to_shapekey_idx(self, target, ll_idx):
+    def livelink_to_shapekey_idx(self, ll_idx):
         name = LIVE_LINK_FACE_HEADER[ll_idx+2]
         for n in [name, name[0].lower() + name[1:]]:
-            idx = target.data.shape_keys.key_blocks.find(n)
+            idx = self.target.data.shape_keys.key_blocks.find(n)
             if idx != -1:
                 return idx
         return idx
@@ -124,11 +52,97 @@ class LiveLinkTarget:
         name = LIVE_LINK_FACE_HEADER[ll_idx+2]
         for n in [name, name[0].lower() + name[1:]]:
             try:
-                target[n]
+                self.target[n]
                 return n
             except:
                 pass
-        return None        
+        return None    
+            
+    '''Sets the value for the LiveLink blendshape at index [i_ll] to [val] for frame [frame] (note the underlying target may be a blendshape or a bone).'''
+    def set_frame_value(self, i_ll, val, frame=0):
+        i_sk = self.livelink_to_shapekey_idx(i_ll)
+        
+        if i_sk != -1:
+            self.sk_frames[frame][i_sk] = val
+        else:
+            bone_prop = self.livelink_to_bone_prop(i_ll)
+            if bone_prop is not None:
+                bone_idx =self.bone_props.index(bone_prop)
+                self.bone_frames[frame][bone_idx] = val
+            else:
+#                print(f"Failed to set bone prop for {i_ll}")
+                pass
+
+    '''Loads a CSV in LiveLinkFace format. First line is the header (Timecode,BlendshapeCount,etc,etc), every line thereafter is a single frame with comma-separated weights'''
+    @staticmethod
+    def from_csv(target,path,action_name,use_first_frame_as_zero=False):        
+        csvdata = [x for x in csv.reader(open(path,"r"))]
+        num_frames = len(csvdata) - 1
+        
+        target = LiveLinkTarget(target, num_frames, action_name=action_name)
+
+        for idx,blendshape in enumerate(LIVE_LINK_FACE_HEADER):
+            if idx < 2:
+                continue
+            
+            rest_weight = float(csvdata[1][idx])
+            
+            for i in range(1, num_frames):
+                val = float(csvdata[i][idx])
+                if use_first_frame_as_zero:
+                    val -= rest_weight
+                target.set_frame_value(idx-2, val,frame=i-1)
+        
+        target.update_animation()
+        return target
+    
+    def update_animation(self):
+        # a bit slow to use bpy.context.object.data.shape_keys.keyframe_insert(datapath,frame=frame)
+        # (where datapath is 'key_blocks["MouthOpen"].value') 
+        # better to add a new fcurve for each shape key then set the points in one go        
+        frame_nums = list(range(len(self.sk_frames)))
+        for i_sk,fc in enumerate(self.sk_fcurves):
+            frame_values = [self.sk_frames[i][i_sk] for i in frame_nums]
+            frame_data = [x for co in zip(frame_nums, frame_values) for x in co]
+            fc.keyframe_points.foreach_set('co',frame_data)
+            
+        for i_b,fc, in enumerate(self.bone_fcurves):
+            frame_values = [self.bone_frames[i][i_b] for i in frame_nums]
+            frame_data = [x for co in zip(frame_nums, frame_values) for x in co]
+            fc.keyframe_points.foreach_set('co',frame_data)
+       
+    def create_action(self, action_name):
+    
+        # create a new Action so we can directly create fcurves and set the keyframe points
+        self.sk_action = bpy.data.actions.new(f"{action_name}_sk") 
+        self.bone_action = bpy.data.actions.new(f"{action_name}_bone") 
+    
+        # create the bone AnimData if it doesn't exist 
+        if self.target.animation_data is None:
+            self.target.animation_data_create()
+            
+        self.target.animation_data.action = self.bone_action
+            
+        # create the shape key AnimData if it doesn't exist 
+        if self.target.data.shape_keys.animation_data is None:
+            self.target.data.shape_keys.animation_data_create()
+            
+        self.target.data.shape_keys.animation_data.action = self.sk_action
+        
+        self.sk_fcurves = []
+        self.bone_fcurves = []
+        
+        for sk in self.target.data.shape_keys.key_blocks:
+            datapath = f"{sk.path_from_id()}.value"
+            fc = self.sk_action.fcurves.new(datapath)
+            fc.keyframe_points.add(count=len(self.sk_frames))
+            self.sk_fcurves += [fc]
+
+        for bone_prop in self.bone_props:
+            fc = self.bone_action.fcurves.new("{bone_prop}_fcurve")
+            fc.keyframe_points.add(count=len(self.sk_frames))
+            self.bone_fcurves += [fc]
+    
        
     def update_to_frame(self, frame=0):
         self.target.data.shape_keys.key_blocks.foreach_set("value", self.sk_frames[frame])        
@@ -138,11 +152,11 @@ class LiveLinkTarget:
 
 class LiveLinkFaceServer:
 
-    def __init__(self, target, host, udp_port):
+    def __init__(self, targets, host, udp_port):
         self.host = host
         self.port = udp_port
         self.millis = int(round(time.time() * 1000))
-        self.target = LiveLinkTarget(target)
+        self.targets = [ LiveLinkTarget(x,num_frames=1) for x in targets ]
         
         bpy.app.timers.register(self.handle_data)
         self.create_socket()
@@ -170,10 +184,11 @@ class LiveLinkFaceServer:
                 return interval
             success, live_link_face = PyLiveLinkFace.decode(data)
             if success:
-                for i in range(len(FaceBlendShape)):
-                    val = live_link_face.get_blendshape(FaceBlendShape(i))
-                    self.target.set_frame_value(i, val, frame=0)
-                self.target.update_to_frame(0)
+                for t in self.targets:
+                    for i in range(len(FaceBlendShape)):
+                        val = live_link_face.get_blendshape(FaceBlendShape(i))
+                        t.set_frame_value(i, val, frame=0)
+                    t.update_to_frame(0)
 
         except Exception as e:
             print(e)
